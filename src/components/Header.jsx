9d1fc93eee28396791ed7dab9808da9fbacfbd3b
alexander <alexander.vdbroeck@gmail.com>
2020-04-13 17:01:36 +0200

    header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import Logo from "../img/bol_logo.jpg";
+import Logo from "../img/header_salm.jpg";
+import {
+  Grid,
+  Box,
+  Typography,
+  
+
+} from "@material-ui/core";
 
 export default props => {
   return (
     <header>
-      <img src={Logo} alt="logo" height="100" />
+      <Grid container spacing={2}
+      justify="center">
+        <Grid item ms={6}><img src={Logo} alt="logo" height="200" /></Grid>
+      <Grid item ms={6}>     <Typography component="div">
+      <Box noWrap letterSpacing={6} m={5}>
+        Njamie Njamie
+      </Box>
+    </Typography></Grid>
+      </Grid>
+      
+ 
     </header>
   );
 };
